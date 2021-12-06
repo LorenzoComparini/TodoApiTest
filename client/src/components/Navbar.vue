eadabaf89e2f637230e7645190cb646719d0c408
LorenzoComparini <lorenzo.comparini01@gmail.com>
2021-12-06 11:44:15 +0100

    Installed vue packages

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex w-full bg-blue-500 justify-between px-5 py-8 text-white font-semibold">
+      <p class="text-2xl">Toododooo</p>
+      <div>
+          <router-link class="text-xl" to="">Link</router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './style.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+import Home from '@/components/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home 
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
